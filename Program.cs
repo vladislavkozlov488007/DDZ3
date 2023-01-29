bb24e837e92cf421813110f897e63004348d5c4a
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-01-29 16:52:09 +0300

    dz23

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿System.Console.WriteLine("Введите координаты первой точки");
-int x1 = int.Parse(Console.ReadLine());
-int x2 = int.Parse(Console.ReadLine());
-int x3 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите координаты второй точки");
-int y1 = int.Parse(Console.ReadLine());
-int y2 = int.Parse(Console.ReadLine());
-int y3 = int.Parse(Console.ReadLine());
-
-double result = Math.Round(Math.Sqrt(Math.Pow((x1-y1),2) + Math.Pow((x2-y2),2) + Math.Pow((x3-y3),2)),2);
-System.Console.WriteLine($"Расстояние между первой и второй точкой равно {result}");
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<=n; i++)
+{
+    System.Console.Write($"{i*i*i} ");
+}
+System.Console.WriteLine();
